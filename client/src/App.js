9cb6ef88d8e04f77be7029e081713909b3ad0189
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-26 23:21:34 +0530

    creating landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
