f9e25daaa499a7f0b8f2ce05ef639c70d2a85a2f
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-27 19:19:27 +0530

    landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./css/home.css";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <h1>Home Page</h1>
+    <div className="home">
+      <div className="home-logo">
+        <h1>WatchMOJO</h1>
+      </div>
+      <div className="home-landing">
+        <p id="msg1">Unlimited Entertainment</p>
+        <p id="msg2">Binge Watch your favorite shows </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
