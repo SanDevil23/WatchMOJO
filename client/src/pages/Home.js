7913b2a7f9fd7e8b2455f1f33f62a4c47481c4ad
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-30 01:12:08 +0530

    landing page completed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,12 +4,20 @@ import "./css/home.css";
 const Home = () => {
   return (
     <div className="home">
-      <div className="home-logo">
-        <h1>WatchMOJO</h1>
-      </div>
+      <div className="background"></div>
       <div className="home-landing">
-        <p id="msg1">Unlimited Entertainment</p>
-        <p id="msg2">Binge Watch your favorite shows </p>
+        <div className="topbar">
+          <h1 id="brand">WatchMOJO</h1>
+          <div className="btn-med">
+            <button>Subscribe</button>
+            <button>Login</button>
+          </div>
+        </div>
+        <div className="msg-box">
+          <p id="msg1">Unlimited Entertainment</p>
+          <p id="msg2">Binge Watch your favorite shows and movies </p>
+          <button id="prv">Get Started </button>
+        </div>
       </div>
     </div>
   );
